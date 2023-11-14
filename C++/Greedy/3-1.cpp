4db3bd049eacb84b364d0378eec71ed787262c30
Eric Kim <48634064+hyeonukim@users.noreply.github.com>
2023-11-14 08:51:28 -0800

    edit 3-1.cpp

--- C++/Greedy/3-1.cpp
+++ C++/Greedy/3-1.cpp
@@ -6,9 +6,9 @@
 using namespace std;
 
 // n:            how much money customer wants
-// count:        how much bill customer gets
+// cnt:        how much bill customer gets
 int n = 1260;
-int count = 0;
+int cnt = 0;
 
 // coin_types:   how much each coin/bill customer can get
 // note that we start from highest coin/bill type (500 >> 100 >> 50 >> 10)
@@ -17,9 +17,9 @@ int coin_types[4] = {500, 100, 50, 10};
 int main() {
     for (int i = 0; i <= 4; i++){
         int coin = coin_types[i];
-        count += n / coin;
+        cnt += n / coin;
         n %= coin;
     }
 
-    cout << count << '\n';
+    cout << cnt << '\n';
 }
