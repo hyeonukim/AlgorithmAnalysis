ccd84333084026d37c4f6967862e8e8bef9592f2
Eric Kim <48634064+hyeonukim@users.noreply.github.com>
2023-11-14 08:50:46 -0800

    3-1 Greedy

---
+++ C++/Greedy/3-1.cpp
@@ -0,0 +1,25 @@
+//Time complexity:   O(K) where K represents the number of types of coin/bill
+//Space complexity:  O(1)
+
+#include <iostream>
+
+using namespace std;
+
+// n:            how much money customer wants
+// count:        how much bill customer gets
+int n = 1260;
+int count = 0;
+
+// coin_types:   how much each coin/bill customer can get
+// note that we start from highest coin/bill type (500 >> 100 >> 50 >> 10)
+int coin_types[4] = {500, 100, 50, 10};
+
+int main() {
+    for (int i = 0; i <= 4; i++){
+        int coin = coin_types[i];
+        count += n / coin;
+        n %= coin;
+    }
+
+    cout << count << '\n';
+}
---
+++ Python/Greedy/3-1.py
@@ -0,0 +1,17 @@
+#Time complexity:   O(K) where K represents the number of types of coin/bill
+#Space complexity:  O(1)
+
+# n:            how much money customer wants
+# count:        how much bill customer gets
+n = 1260
+count = 0
+
+# coin_types:   how much each coin/bill customer can get
+# note that we start from highest coin/bill type (500 >> 100 >> 50 >> 10)
+coin_types = [500, 100, 50, 10]
+
+for coin in coin_types:
+    count += n // coin
+    n %= coin
+
+print(count)
