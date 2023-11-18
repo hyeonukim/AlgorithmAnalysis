e7b36b7880b12ec5855428145eb495b5372a1aa3
Eric Kim <48634064+hyeonukim@users.noreply.github.com>
2023-11-18 13:10:06 -0800

    11-5.cpp added

--- C++/Greedy/11-5.cpp
+++ C++/Greedy/11-5.cpp
@@ -7,3 +7,39 @@ EX) N = 5, M = 3, arr = 1,3,2,3,2
 
 ans = (1,3), (1,2), (1,3), (1,2), (3,2), (3,2), (2,3), (3,2) - therefore there are 8 ways
 */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int m;
+vector<int> arr;
+
+int main(){
+    cin >> n >> m;
+    vector<int> count;
+
+    //create arr, and initialize the count array
+    for (int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        arr.push_back(x);
+        count.push_back(0);
+    }
+    count.push_back(0);
+    
+    //get the count of each weights
+    for (int i = 0; i < n; i++){
+        count[arr[i]]++;
+    }
+
+    int res = 0;
+    //for each count of the same weight * rest of the count
+    for (int i = 0; i < m; i++){
+        n -= count[i];
+        res += count[i] * n;
+    }
+
+    cout << res << '\n';
+}
