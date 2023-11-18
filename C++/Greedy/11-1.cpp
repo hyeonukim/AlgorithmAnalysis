ec1090fdbc96749aa94ba15147405190704a55cd
Eric Kim <48634064+hyeonukim@users.noreply.github.com>
2023-11-18 07:21:24 -0800

    11-1.py added

---
+++ C++/Greedy/11-1.cpp
@@ -0,0 +1,16 @@
+/*
+Question:
+    In a vaillage, there are N adventurers. In this adventure guild, they measured level of 'fear' for N adventurers.
+    the higher level the 'fear' is, they perfrom less in dangerous situation. The guild master Dongbin tries to make
+    a group as safe/efficient as possible by "If a member's level of 'fear' is X then there must be more or equal to 
+    X people in the group for the adventure. Dongbin is curious how many groups he can make with the members.
+    Create a code for Dongbin given N amount of people and information about them, that calculates how many total groups
+    are possible.
+
+EX)
+    If N = 5, with each of their level of 'fear' being '2 3 1 2 2'
+    then 
+    Group 1: 1,2,3 Group 2: 2,2
+    OR
+    Group 1: 1, Group 2: 2,2 since we can always leave 2,3 in village
+*/
